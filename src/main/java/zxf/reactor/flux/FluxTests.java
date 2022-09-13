6991a517aa4f51551fed66196099dedf60df3fe6
davis <zanxiaofeng@163.com>
2022-09-14 07:13:14 +0800

    update

--- src/main/java/zxf/reactor/flux/FluxTests.java
+++ src/main/java/zxf/reactor/flux/FluxTests.java
@@ -2,17 +2,61 @@ package zxf.reactor.flux;
 
 import reactor.core.publisher.Flux;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class FluxTests {
-    public static void main(String[] args) throws InterruptedException {
-        flux_case_1();
+    public static void main(String[] args) {
+        case_1();
+        case_2();
+        case_3();
     }
 
-    public static void flux_case_1() throws InterruptedException {
-        Flux<Integer> flux = Flux.just(1, 2, 3, 4);
-        Flux<Integer> flux1 = flux.map(x -> x * x);
-        flux1.subscribe(x -> System.out.println("*" + x));
-        Thread.sleep(4000);
-        Flux<Integer> flux2 = flux.map(x -> x + x);
-        flux2.subscribe(x -> System.out.println("+" + x));
+    private static void case_1() {
+        System.out.println("case 1: onNext|onNext|onComplete");
+        Flux.just(1, 2).subscribe(x -> {
+            System.out.println("onNext: " + x);
+        }, e -> {
+            System.out.println("onError: will not be touched");
+        }, () -> {
+            System.out.println("onComplete: case 1");
+        });
+    }
+
+    private static void case_2() {
+        System.out.println("case 2: onComplete");
+        Flux.empty().subscribe(x -> {
+            System.out.println("onNext: will not be touched");
+        }, e -> {
+            System.out.println("onError: will not be touched");
+        }, () -> {
+            System.out.println("onComplete: case 2");
+        });
+    }
+
+    private static void case_3() {
+        System.out.println("case 3: onNext|onNext|onError");
+        AtomicInteger initial = new AtomicInteger();
+        Flux.generate(synchronousSink -> {
+            Integer current = initial.incrementAndGet();
+            if (current < 3) {
+                synchronousSink.next(current);
+                return;
+            }
+            if (current == 3) {
+                synchronousSink.error(new RuntimeException("case 3"));
+                return;
+            }
+            if (current < 6) {
+                synchronousSink.next(current);
+                return;
+            }
+            synchronousSink.complete();
+        }).subscribe(x -> {
+            System.out.println("onNext: " + x);
+        }, e -> {
+            System.out.println("onError: " + e.toString());
+        }, () -> {
+            System.out.println("onComplete: will not be touched");
+        });
     }
 }
--- src/main/java/zxf/reactor/mono/MonoTests.java
+++ src/main/java/zxf/reactor/mono/MonoTests.java
@@ -4,47 +4,41 @@ import reactor.core.publisher.Mono;
 
 public class MonoTests {
     public static void main(String[] args) {
-        Mono.empty()
-                .subscribe(x -> System.out.println("case 1: will not touch"));
+        case_1();
+        case_2();
+        case_3();
+    }
 
-        Mono.empty()
-                .map(x -> {
-                    System.out.println("case 2: will not touch");
-                    return "test";
-                })
-                .subscribe();
+    private static void case_1() {
+        System.out.println("case 1: onNext|onComplete");
+        Mono.just("test").subscribe(x -> {
+            System.out.println("onNext: " + x);
+        }, e -> {
+            System.out.println("onError: will not be touched");
+        }, () -> {
+            System.out.println("onComplete: case 1");
+        });
+    }
 
-        Mono.empty().then()
-                .subscribe(x -> System.out.println("case 3: will not touch"));
+    private static void case_2() {
+        System.out.println("case 2: onComplete");
+        Mono.empty().subscribe(x -> {
+            System.out.println("onNext: will not be touched");
+        }, e -> {
+            System.out.println("onError: will not be touched");
+        }, () -> {
+            System.out.println("onComplete: case 2");
+        });
+    }
 
-        Mono.empty().then()
-                .map(x -> {
-                    System.out.println("case 4: will not touch");
-                    return "test";
-                })
-                .subscribe();
-
-        Mono.empty().then()
-                .doOnSuccess(x -> {
-                    System.out.println("case 5: " + x);
-                })
-                .subscribe();
-
-        Mono.empty().thenReturn("test")
-                .map(x -> {
-                    System.out.println("case 6: " + x);
-                    return x + "-" + x;
-                })
-                .subscribe();
-
-        Mono.just("test")
-                .subscribe(x -> System.out.println("case 7: " + x));
-
-        Mono.just("test").then()
-                .subscribe(x -> System.out.println("case 8: will not touch" + x));
-
-        Mono.just("test").then()
-                .doOnNext(x -> System.out.println("case 9: will not touch" + x))
-                .subscribe();
+    private static void case_3() {
+        System.out.println("case 3: onError");
+        Mono.error(new RuntimeException("case 3")).subscribe(x -> {
+            System.out.println("onNext: will not be touched");
+        }, e -> {
+            System.out.println("onError: " + e.toString());
+        }, () -> {
+            System.out.println("onComplete: will not be touched");
+        });
     }
 }
