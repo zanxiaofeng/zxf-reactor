a2375fb5c406fc694361437fd00b82a0c8720262
davis <zanxiaofeng@163.com>
2023-08-30 10:31:56 +0800

    update

--- src/main/java/zxf/reactor/core/CoreTests.java
+++ src/main/java/zxf/reactor/core/CoreTests.java
@@ -5,96 +5,205 @@ import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
 
 public class CoreTests {
+    private static AtomicInteger atomicInteger = new AtomicInteger();
+
     public static void main(String[] args) {
-        Publisher<Integer> publisher = new Publisher<Integer>() {
-            private Integer seed = 0;
+        MyPublisher<Integer> myPublisher = new MyPublisher(20, atomicInteger::incrementAndGet);
 
-            @Override
-            public void subscribe(Subscriber<? super Integer> subscriber) {
-                this.seed += 1;
-                subscriber.onSubscribe(new Subscription() {
-                    Integer current = seed;
+        MyFilterProcessor<Integer> myFilterProcessor = new MyFilterProcessor<>(myPublisher, x -> x % 2 == 0);
 
-                    @Override
-                    public void request(long l) {
-                        subscriber.onNext(current);
-                        subscriber.onComplete();
-                    }
+        MyMapProcessor<Integer, String> myMapProcessor = new MyMapProcessor<>(myFilterProcessor, x -> {
+            return "**" + x + "**";
+        });
 
-                    @Override
-                    public void cancel() {
+        MySubscriber mySubscriber = new MySubscriber(value -> {
+            System.out.println(Thread.currentThread() + "  onNext::" + value + ".");
+        });
+        myMapProcessor.subscribe(mySubscriber);
+    }
 
-                    }
-                });
-            }
-        };
+    public static class MyPublisher<T> implements Publisher<T> {
+        private Integer limit;
+        private Supplier<T> supplier;
 
-        Processor<Integer, String> processor = new Processor<Integer, String>() {
-            private Subscription subscription;
-            private Subscriber<? super String> subscriber;
+        public MyPublisher(Integer limit, Supplier<T> supplier) {
+            System.out.println(Thread.currentThread() + "  MyPublisher::cotr");
+            this.limit = limit;
+            this.supplier = supplier;
+        }
 
-            @Override
-            public void subscribe(Subscriber<? super String> subscriber) {
-                this.subscriber = subscriber;
-                subscriber.onSubscribe(new Subscription() {
-                    @Override
-                    public void request(long l) {
-                        subscription.request(l);
-                    }
+        @Override
+        public void subscribe(Subscriber<? super T> subscriber) {
+            System.out.println(Thread.currentThread() + "  MyPublisher::subscribe." + limit);
+            subscriber.onSubscribe(new MySubscription(limit, supplier, subscriber));
+        }
+    }
 
-                    @Override
-                    public void cancel() {
+    public static class MySubscription<T> implements Subscription {
+        private Integer limit;
+        private Supplier<T> supplier;
+        ;
+        private Subscriber<? super T> subscriber;
 
-                    }
-                });
-            }
+        public MySubscription(Integer limit, Supplier<T> supplier, Subscriber<? super T> subscriber) {
+            System.out.println(Thread.currentThread() + "  MySubscription::cotr");
+            this.limit = limit;
+            this.supplier = supplier;
+            this.subscriber = subscriber;
+        }
 
-            @Override
-            public void onSubscribe(Subscription subscription) {
-                this.subscription = subscription;
-            }
-
-            @Override
-            public void onNext(Integer integer) {
-                subscriber.onNext(integer + "-" + integer);
+        @Override
+        public void request(long count) {
+            System.out.println(Thread.currentThread() + "  MySubscription::request." + limit);
+            if (limit <= 0){
+                return;
             }
 
-            @Override
-            public void onError(Throwable error) {
-                subscriber.onError(error);
+            for (int i = 0; i < count; i++) {
+                limit--;
+                subscriber.onNext(supplier.get());
             }
 
-            @Override
-            public void onComplete() {
+            if (limit <= 0) {
                 subscriber.onComplete();
             }
-        };
+        }
 
-        Subscriber<String> subscriber = new Subscriber<String>() {
-            @Override
-            public void onSubscribe(Subscription subscription) {
-                subscription.request(1000);
+        @Override
+        public void cancel() {
+            System.out.println(Thread.currentThread() + "  MySubscription::cancel." + limit);
+            subscriber.onComplete();
+        }
+    }
+
+    public static class MyMapProcessor<T, R> implements Processor<T, R> {
+        private Publisher<T> source;
+        private Function<T, R> mapper;
+        private Subscriber<? super R> actual;
+
+        public MyMapProcessor(Publisher<T> source, Function<T, R> mapper) {
+            System.out.println(Thread.currentThread() + "  MyMapProcessor::cotr");
+            this.source = source;
+            this.mapper = mapper;
+        }
+
+        @Override
+        public void subscribe(Subscriber<? super R> actual) {
+            System.out.println(Thread.currentThread() + "  MyMapProcessor::subscribe");
+            this.actual = actual;
+            source.subscribe(this);
+        }
+
+        @Override
+        public void onSubscribe(Subscription subscription) {
+            System.out.println(Thread.currentThread() + "  MyMapProcessor::onSubscribe");
+            actual.onSubscribe(subscription);
+        }
+
+        @Override
+        public void onNext(T value) {
+            System.out.println(Thread.currentThread() + "  MyMapProcessor::onNext." + value);
+            actual.onNext(mapper.apply(value));
+        }
+
+
+        @Override
+        public void onError(Throwable error) {
+            System.out.println(Thread.currentThread() + "  MyMapProcessor::onError." + error);
+            actual.onError(error);
+        }
+
+        @Override
+        public void onComplete() {
+            System.out.println(Thread.currentThread() + "  MyMapProcessor::onComplete");
+            actual.onComplete();
+        }
+    }
+
+    public static class MyFilterProcessor<T> implements Processor<T, T> {
+        private Publisher<T> source;
+        private Predicate<T> predicate;
+        private Subscriber<? super T> actual;
+
+        public MyFilterProcessor(Publisher<T> source, Predicate<T> predicate) {
+            System.out.println(Thread.currentThread() + "  MyFilterProcessor::cotr");
+            this.source = source;
+            this.predicate = predicate;
+        }
+
+        @Override
+        public void subscribe(Subscriber<? super T> actual) {
+            System.out.println(Thread.currentThread() + "  MyFilterProcessor::subscribe");
+            this.actual = actual;
+            source.subscribe(this);
+        }
+
+        @Override
+        public void onSubscribe(Subscription subscription) {
+            System.out.println(Thread.currentThread() + "  MyFilterProcessor::onSubscribe");
+            actual.onSubscribe(subscription);
+        }
+
+        @Override
+        public void onNext(T value) {
+            System.out.println(Thread.currentThread() + "  MyFilterProcessor::onNext." + value);
+            if (predicate.test(value)) {
+                actual.onNext(value);
             }
+        }
 
-            @Override
-            public void onNext(String next) {
-                System.out.println("onNext::" + next + ".");
-            }
 
-            @Override
-            public void onError(Throwable throwable) {
-                System.out.println("onError::" + throwable + ".");
-            }
+        @Override
+        public void onError(Throwable error) {
+            System.out.println(Thread.currentThread() + "  MyFilterProcessor::onError." + error);
+            actual.onError(error);
+        }
 
-            @Override
-            public void onComplete() {
-                System.out.println("onComplete.");
-            }
-        };
+        @Override
+        public void onComplete() {
+            System.out.println(Thread.currentThread() + "  MyFilterProcessor::onComplete");
+            actual.onComplete();
+        }
+    }
 
-        publisher.subscribe(processor);
-        processor.subscribe(subscriber);
+    public static class MySubscriber<T> implements Subscriber<T> {
+        private Consumer<T> consumer;
+        private Subscription subscription;
+
+        public MySubscriber(Consumer<T> consumer) {
+            System.out.println(Thread.currentThread() + "  MySubscriber::cotr");
+            this.consumer = consumer;
+        }
+
+        @Override
+        public void onSubscribe(Subscription subscription) {
+            System.out.println(Thread.currentThread() + "  MySubscriber::onSubscribe");
+            this.subscription = subscription;
+            subscription.request(100);
+        }
+
+        @Override
+        public void onNext(T value) {
+            System.out.println(Thread.currentThread() + "  MySubscriber::onNext." + value);
+            this.consumer.accept(value);
+        }
+
+        @Override
+        public void onError(Throwable throwable) {
+            System.out.println(Thread.currentThread() + "  MySubscriber::onError::" + throwable + ".");
+        }
+
+        @Override
+        public void onComplete() {
+            System.out.println(Thread.currentThread() + "  MySubscriber::onComplete.");
+        }
     }
 }
